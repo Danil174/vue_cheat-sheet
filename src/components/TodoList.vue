ec43e73dd6e6c0325b850d79bf407f52ee845cac
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-27 19:49:01 +0300

    actions

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,6 +5,7 @@
                 v-for="todo of todos"
                 v-bind:todo="todo"
                 :key="todo.id"
+                v-on:remove-todo="removeTodo"
             />
         </ul>
     </div>
@@ -16,6 +17,11 @@
         props: ['todos'],
         components: {
             TodoItem
+        },
+        methods: {
+            removeTodo(id) {
+                this.$emit('remove-todo', id)
+            }
         }
     }
 </script>
